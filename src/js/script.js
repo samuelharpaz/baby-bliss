572540ab3943aff941aea0174d39138f8b2bbb3a
Samuel Harpaz <sharpaz24@gmail.com>
2025-06-22 18:53:03 -0400

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// SLIDER
 const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
@@ -16,3 +17,34 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 96
 });
+
+// ACCORDION
+
+const accordion = document.querySelector('.accordion');
+
+accordion.addEventListener('click', function (e) {
+  const activePanel = e.target.closest('.accordion-panel');
+  if (!activePanel) return;
+
+  toggleAccordion(activePanel);
+});
+
+function toggleAccordion(panelToActivate) {
+  const buttons = panelToActivate.parentElement.querySelectorAll('button');
+  const contents = panelToActivate.parentElement.querySelectorAll('.accordion-content');
+
+  const isClosed = panelToActivate.querySelector('button').getAttribute('aria-expanded') === 'false';
+
+  buttons.forEach(button => {
+    button.setAttribute('aria-expanded', false);
+  });
+
+  contents.forEach(content => {
+    content.setAttribute('aria-hidden', true);
+  });
+
+  if (isClosed) {
+    panelToActivate.querySelector('button').setAttribute('aria-expanded', true);
+    panelToActivate.querySelector('.accordion-content').setAttribute('aria-hidden', false);
+  }
+}
