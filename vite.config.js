b9dcdffe1de95c08293eb0ba92b9ee52219266f2
Samuel Harpaz <sharpaz24@gmail.com>
2025-07-06 17:23:48 -0400

    remove .html from links, set up vite config for multiple pages, place js in public

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,21 @@ export default {
     handlebars({
       partialDirectory: resolve(__dirname, 'partials')
     })
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './about-me.html',
+        classes: './classes.html',
+        faves: './favorite-products.html',
+        contact: './contact.html',
+        virtualForm: './virtual-sessions.html',
+        homeForm: './home-sessions.html',
+        scheduleCallForm: './schedule-call.html',
+        followUpForm: './follow-up-session.html',
+        fatherSupportForm: './father-support.html'
+      }
+    }
+  }
 };
